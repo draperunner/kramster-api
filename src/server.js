f27fefca52ef6062284b7bcc27421932c8007257
Mats Byrkjeland <matsbyr@gmail.com>
2018-11-29 17:43:49 +0100

    Add cors support

--- src/server.js
+++ src/server.js
@@ -2,12 +2,23 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const path = require('path')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 // MongoDB
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (['https://kramster.it', 'https://kramsterapp.firebaseapp.com'].includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}))
 
 app.disable('x-powered-by')
 app.use(bodyParser.urlencoded({ extended: true }))
