1f6df8236f701dfdc568abbe77c1529365b133ca
Mats Byrkjeland <matsbyr@gmail.com>
2018-11-30 00:34:24 +0100

    Do not send history for individual questions to user

--- src/api/questions/question.model.js
+++ src/api/questions/question.model.js
@@ -6,11 +6,14 @@ const questionSchema = new mongoose.Schema({
   options: [String],
   answers: [Number],
   explanation: String,
-  history: [{
-    _id: false, // Prevent mongoose from automatically creating ids for subdocuments
-    givenAnswer: String,
-    wasCorrect: Boolean,
-  }],
+  history: {
+    type: [{
+      _id: false, // Prevent mongoose from automatically creating ids for subdocuments
+      givenAnswer: String,
+      wasCorrect: Boolean,
+    }],
+    select: false,
+  },
   stats: {
     totalAnswers: Number, // Should equal history.length
     totalCorrect: Number, // Should equal history.filter(q => wasCorrect).length
