7ce5cd5ef28c5f92096ef0510c08c4bd378668df
Mats Byrkjeland <matsbyr@gmail.com>
2019-02-19 23:51:08 +0100

    Update deploy scripts

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,6 +2,6 @@ npm run build
 echo "Compressing..."
 tar -zcf dist.tar.gz dist package.json package-lock.json
 echo "Sending..."
-scp dist.tar.gz kramster:kramster-api-staging/
+scp dist.tar.gz byrkje:kramster-api-staging/
 echo "Running server staging deploy script"
-ssh -t kramster './deploy-kramster-api-staging.sh'
+ssh -t byrkje './deploy-kramster-api-staging.sh'
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ npm run build
 echo "Compressing..."
 tar -zcf dist.tar.gz dist package.json package-lock.json
 echo "Sending..."
-scp dist.tar.gz kramster:kramster-api/
+scp dist.tar.gz byrkje:kramster-api/
 echo "Running server prod deploy script"
-ssh -t kramster './deploy-kramster-api.sh'
+ssh -t byrkje './deploy-kramster-api.sh'
